f7e9efea6876b2facc2d6d1aec58241cf1f4daa7
DCBP <DCBP@DCBP-iMac-21in-1.local>
2017-03-17 18:01:25 +0800

    codewar

--- JavaScript_practice.js
+++ JavaScript_practice.js
@@ -1,18 +1,9 @@
 function getBinary(anInteger){
-    var result = ""
-    var shortResult = ""
-    for(var i = 1;i < 33;i ++){
-        if(anInteger & 1 == 1){
-            result = "1" + result
-            shortResult = result
-        }else{
-            result = "0" +result
-        }
-        anInteger = anInteger >> 1
-        console.log(anInteger+" "+result+" "+shortResult);
-    }
-    return (shortResult)
+    return anInteger.toString(2);
 }
+
+
+
 
 
 function Clock(hours, minutes){
